4b5cdab86ae8b9a706fdaa8ebe7bbf62f112cac6
zahngsan <zhangsan@163.com>
2019-11-17 18:54:37 +0800

    提交到GitHub远程仓库

--- git/src/test.java
+++ git/src/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("IDEA第二次提交");
+        System.out.println("第一次克隆修改操作");
     }
 
 
