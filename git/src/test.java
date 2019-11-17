4271a29adb3ce46b8eb4ab7d45d0a10f20927fd5
lizhipeng1111 <57855183+lizhipeng1111@users.noreply.github.com>
2019-11-17 18:56:11 +0800

    Update test.java

--- git/src/test.java
+++ git/src/test.java
@@ -3,6 +3,9 @@ public class test {
         System.out.println("HelloWorld");
         System.out.println("IDEA第二次提交");
         System.out.println("第一次克隆修改操作");
+         System.out.println("第二次克隆修改操作");
+        
+
     }
 
 
